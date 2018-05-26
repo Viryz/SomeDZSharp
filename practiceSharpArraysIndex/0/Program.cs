c17ccfaa0051a0062c319e95882ecf4275e95422
Inna Balabon <1inna.balabon@gmail.com>
2018-05-26 20:30:44 +0300

    0 task

--- practiceSharpArraysIndex/0/Program.cs
+++ practiceSharpArraysIndex/0/Program.cs
@@ -10,7 +10,29 @@ namespace _0
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine("Enter the number of elements");
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            Random r = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(1, 100);
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Max: {0}", array.Max());
+            Console.WriteLine("Min: {0}", array.Min());
+            Console.WriteLine("Sum: {0}", array.Sum());
+            Console.WriteLine("Avg: {0}", array.Average());
 
+            Console.WriteLine("Numbers (% 2 == 1):");
+
+            IEnumerable<int> newArr = from item in array where item % 2 == 1 select item;
+            foreach (int item in newArr)
+                Console.Write(item + " ");
+            Console.WriteLine();
+            Console.ReadLine();
         }
     }
 }
